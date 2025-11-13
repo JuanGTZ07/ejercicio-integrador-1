63c7d44c9dddcd5bebdb0206520cf571ed668f13
JuanGTZ07 <al45677@un.edu.mx>
2025-11-13 09:46:13 -0600

    👍

--- javascript.js
+++ javascript.js
@@ -1,5 +1,26 @@
 var formulario = document.getElementById("formulario");
 
 function calcular() {
+    var total = 0;
+    var paquetes = formulario.paquetes.value;
+
+    if (paquetes == 850){
+        total += 850;
+    }
+    else if (paquetes == 700) {
+        total += 700;
+    }
+    else if (paquetes == 2000) {
+        total += 2000;
+    }
+    else if (paquetes == 5000) {
+        total += 5000;
+    }
+    else {
+        alert("Escoja un paquete")
+    }
+
+    document.getElementById('total').innerText = total;
     
+    alert("Gracias por tu interes");
 }
