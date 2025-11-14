0541dcd68558b25b44d6405e8410cda715d4002a
JuanGTZ07 <al45677@un.edu.mx>
2025-11-13 19:28:46 -0600

    avance

--- javascript.js
+++ javascript.js
@@ -3,6 +3,9 @@ var formulario = document.getElementById("formulario");
 function calcular() {
     var total = 0;
     var paquetes = formulario.paquetes.value;
+    var analisis = formulario.analisis.value;
+    var prepa = false;
+    var pago = formulario.pago.value;
 
     if (paquetes == 850){
         total += 850;
@@ -17,8 +20,44 @@ function calcular() {
         total += 5000;
     }
     else {
-        alert("Escoja un paquete")
+        alert("Escoja un paquete");
     }
+
+    if (analisis == "sinconsulta") {
+        total += 200;
+    }
+    else if (analisis == "conconsulta") {
+        total += 1000;
+    }
+    else {
+        alert("Escoja un analisis");
+    }
+
+    document.querySelectorAll('input[type="checkbox"]').forEach(prep => {
+        if (prep.checked) {
+            total +=Number(prep.dataset.precio);
+            prepa = true;
+        }
+    } );
+
+    if (!prepa) {
+        alert("escoja una preparacion");
+    }
+
+    if (pago == "efectivo") {
+        total += 0;
+    }
+    else if (pago == "tarjeta") {
+        total += total * 0.6;
+    }
+    else if (pago == "transferencia") {
+        total += total * 0.16;
+    }
+    else {
+        alert("Seleccione su tipo de pago0");
+    }
+
+
 
     document.getElementById('total').innerText = total;
     
