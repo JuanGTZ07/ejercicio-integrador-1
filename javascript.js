699231e7165e31245364e24212bbd33023b9af39
JuanGTZ07 <al45677@un.edu.mx>
2025-11-13 20:20:59 -0600

    final

--- javascript.js
+++ javascript.js
@@ -6,32 +6,17 @@ function calcular() {
     var analisis = formulario.analisis.value;
     var prepa = false;
     var pago = formulario.pago.value;
+    var fac = formulario.FAC.value;
+    var mensaje = document.getElementById("mensaje");
+    var mensaje2 =document.getElementById("mensaje2");
 
-    if (paquetes == 850){
-        total += 850;
-    }
-    else if (paquetes == 700) {
-        total += 700;
-    }
-    else if (paquetes == 2000) {
-        total += 2000;
-    }
-    else if (paquetes == 5000) {
-        total += 5000;
-    }
-    else {
-        alert("Escoja un paquete");
-    }
+    if (paquetes == 850) total += 850;
+    else if (paquetes == 700) total += 700;
+    else if (paquetes == 2000) total += 2000;
+    else if (paquetes == 5000)total += 5000;
 
-    if (analisis == "sinconsulta") {
-        total += 200;
-    }
-    else if (analisis == "conconsulta") {
-        total += 1000;
-    }
-    else {
-        alert("Escoja un analisis");
-    }
+    if (analisis == "sinconsulta") total += 200;
+    else if (analisis == "conconsulta") total += 1000;
 
     document.querySelectorAll('input[type="checkbox"]').forEach(prep => {
         if (prep.checked) {
@@ -40,26 +25,27 @@ function calcular() {
         }
     } );
 
-    if (!prepa) {
-        alert("escoja una preparacion");
-    }
-
     if (pago == "efectivo") {
         total += 0;
     }
     else if (pago == "tarjeta") {
-        total += total * 0.6;
+        total += total * 0.06;
     }
     else if (pago == "transferencia") {
-        total += total * 0.16;
+        total += total * 0.03;
+    }
+
+    if (fac == "si") {
+        var iva = total * 0.16;
+        total += iva;
+        mensaje.textContent = " su IVA es $" + iva.toFixed(2);
     }
     else {
-        alert("Seleccione su tipo de pago0");
+        mensaje.textContent = "";
     }
 
 
-
-    document.getElementById('total').innerText = total;
+    document.getElementById('total').innerText = "total $" + total.toFixed(2);
     
-    alert("Gracias por tu interes");
+    mensaje2.textContent = "Gracias por tu interes";
 }
